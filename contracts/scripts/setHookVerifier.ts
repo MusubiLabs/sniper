2e3caff043990f829b74dd8547d41f34ffb4bb38
Zen Wang <zxc82990@gmail.com>
2024-09-21 16:25:44 +0800

    refactor: seperate Verifier and whitelist

--- contracts/scripts/setHookVerifier.ts
+++ contracts/scripts/setHookVerifier.ts
@@ -11,9 +11,9 @@ async function main() {
         { client: { wallet: deployer } }
     );
 
-    const setWhitelistTx = await sphookAsOwner.write.setWhitelist([deployed_addresses["Sniper#Sniper"], true]);
-    const receipt = await publicClient.waitForTransactionReceipt({ hash: setWhitelistTx });
-    receipt.status === "success" ? console.log('setWhitelist Done:' + setWhitelistTx) : console.log('setWhitelist Failed')
+    const setWorldVerifierTx = await sphookAsOwner.write.setWorldVerifier([deployed_addresses["WorldVerifier#WorldVerifier"]]);
+    const receipt2 = await publicClient.waitForTransactionReceipt({ hash: setWorldVerifierTx });
+    receipt2.status === "success" ? console.log('setWorldVerifier Done:' + setWorldVerifierTx) : console.log('setWorldVerifier Failed')
 
 }
 
